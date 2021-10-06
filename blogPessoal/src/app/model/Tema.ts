71801a30e6d92f8241d38784180b5a631b5d6ecb
Marilia Silva <mariliadevs@gmail.com>
2021-10-06 18:34:31 -0300

    componente inicio sem consumo

--- blogPessoal/src/app/model/Tema.ts
+++ blogPessoal/src/app/model/Tema.ts
@@ -2,7 +2,7 @@ import { Postagem } from "./Postagem";
 
 export class Tema{
 
-    public id: number;
-    public descricao: string;
+    public id: number
+    public descricao: string
     public postagem: Postagem[]
       }
